77f4feb142473712253ed8996190f4807d5bb1e1
Jason DeBacker <jason.debacker@gmail.com>
2026-09-18 16:50:25 -0400

    add row for capital good in IO matrix

--- ogcore/parameters.py
+++ ogcore/parameters.py
@@ -88,11 +88,11 @@ class Specifications(paramtools.Parameters):
         self.J = len(self.lambdas)
 
         io_matrix = np.asarray(self.io_matrix)
-        expected_io_shape = (self.I + 2, self.M)
+        expected_io_shape = (self.I + 3, self.M)
         if io_matrix.shape != expected_io_shape:
             raise ValueError(
                 "io_matrix must have shape "
-                f"(I + 2, M)={expected_io_shape}; got {io_matrix.shape}."
+                f"(I + 3, M)={expected_io_shape}; got {io_matrix.shape}."
             )
         if not np.allclose(io_matrix.sum(axis=1), 1.0):
             raise ValueError("Each row of io_matrix must sum to 1.")
